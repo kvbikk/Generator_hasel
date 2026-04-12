10580483d08b117280db78307f7cc3d3a7bcf29c
klxdak <Klxdak@outlook.com>
2026-04-12 23:23:44 +0200

    naprawa błędu wyświetlacza

--- src/generator.py
+++ src/generator.py
@@ -18,40 +18,44 @@ def generator_hasel(dlugosc_hasla=10, uzyj_cyfr=True, uzyj_specjalnych=True):
 
 
 if __name__ == "__main__":
-    okno = tkinter.Tk()
-    okno.title("Generator hasła")
-    okno.geometry("300x300")
+    try:
+        okno = tkinter.Tk()
+        okno.title("Generator hasła")
+        okno.geometry("300x300")
 
-    tkinter.Label(okno, text="Długość hasła:").pack()
-    dlugosc_entry = tkinter.Entry(okno)
-    dlugosc_entry.pack()
+        tkinter.Label(okno, text="Długość hasła:").pack()
+        dlugosc_entry = tkinter.Entry(okno)
+        dlugosc_entry.pack()
 
-    uzyj_cyfr_var = tkinter.BooleanVar(value=False)
-    tkinter.Checkbutton(okno, text="Użyj cyfr", variable=uzyj_cyfr_var).pack()
+        uzyj_cyfr_var = tkinter.BooleanVar(value=False)
+        tkinter.Checkbutton(okno, text="Użyj cyfr", variable=uzyj_cyfr_var).pack()
 
-    uzyj_specjalnych_var = tkinter.BooleanVar(value=False)
-    tkinter.Checkbutton(okno, text="Użyj specjalnych", variable=uzyj_specjalnych_var).pack()
+        uzyj_specjalnych_var = tkinter.BooleanVar(value=False)
+        tkinter.Checkbutton(okno, text="Użyj specjalnych", variable=uzyj_specjalnych_var).pack()
 
-    haslo_label = tkinter.Label(okno, text="",)
-    haslo_label.pack()
+        haslo_label = tkinter.Label(okno, text="",)
+        haslo_label.pack()
 
-    def generuj():
-        dlugosc = int(dlugosc_entry.get())
-        nowe_haslo = generator_hasel(
-            dlugosc_hasla=dlugosc,
-            uzyj_cyfr=uzyj_cyfr_var.get(),
-            uzyj_specjalnych=uzyj_specjalnych_var.get())
-        haslo_label.config(text=nowe_haslo)
+        def generuj():
+            dlugosc = int(dlugosc_entry.get())
+            nowe_haslo = generator_hasel(
+                dlugosc_hasla=dlugosc,
+                uzyj_cyfr=uzyj_cyfr_var.get(),
+                uzyj_specjalnych=uzyj_specjalnych_var.get())
+            haslo_label.config(text=nowe_haslo)
 
-    moja_dlugosc = 15
+        moja_dlugosc = 15
 
-    if moja_dlugosc is None:
-        nowe_haslo = generator_hasel()
-    else:
-        nowe_haslo = generator_hasel(dlugosc_hasla=moja_dlugosc)
+        if moja_dlugosc is None:
+            nowe_haslo = generator_hasel()
+        else:
+            nowe_haslo = generator_hasel(dlugosc_hasla=moja_dlugosc)
 
-    print(f"Hasło składa się z: {len(nowe_haslo)} znaków")
-    print(f"Twoje nowe hasło to: {nowe_haslo}")
+        print(f"Hasło składa się z: {len(nowe_haslo)} znaków")
+        print(f"Twoje nowe hasło to: {nowe_haslo}")
 
-    tkinter.Button(okno, text="Generuj", command=generuj).pack()
-    okno.mainloop()
+        tkinter.Button(okno, text="Generuj", command=generuj).pack()
+        okno.mainloop()
+    except tkinter.TclError:
+        print("Błąd ekranu")
+        
